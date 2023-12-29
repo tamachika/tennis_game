14366158f53c1b21c312e930ffc0e26fb83b683d
tamachika <62124334+tamachika@users.noreply.github.com>
2023-12-29 09:20:42 +0900

    hitのaudioこれでどうだ

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 //サウンド
 function audio() {
-    document.getElementById('btn_audio').currentTime = 0; //連続クリックに対応
-    document.getElementById('btn_audio').play(); //クリックしたら音を再生
+    document.getElementById('hit_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('hit_audio').play(); //クリックしたら音を再生
 }
 
 
